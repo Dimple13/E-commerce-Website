ea09cb962f31c6428b4b8e98e93782fa7771b937
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2021-04-08 13:01:44 +0530

    payment frontend completed

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -32,6 +32,10 @@ const CheckoutPage=({cartItems,total})=>(
         <div className='total'>
             <span>TOTAL:${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payment.*<br/>
+            4242 4242 4242 4242-Exp:01/22-CVV:123
+        </div>
         <StripeCheckoutButton price={total}/>
     </div>
 );
