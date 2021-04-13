eba7aa11d19e9dd9b93cbe25c55681e84211f4d5
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2021-04-13 16:24:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import HomePage from './pages/homepage/homepage.component' ;
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+
 class App extends React.Component{
   unsubscribeFromAuth=null
   componentDidMount(){
